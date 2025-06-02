4450ad5489f223e8e30026b6be53a77f8fb550d3
Isa Reinert <reinert.isa@gmail.com>
2025-06-01 23:35:45 -0700

    feat(website): dom type casting - 6

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,11 @@
 "use strict";
-// const anchor = document.querySelector('a')!;
-// console.log(anchor.href);
-// const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form');
-console.log(form.children);
+// inputs
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#to-from');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
-// const anchor = document.querySelector('a')!;
-// console.log(anchor.href);
-
-// const form = document.querySelector('form')!;
-
-// const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 // inputs
-const type1 = document.querySelector('#type')!; // type is Element
+const type = document.querySelector('#type') as HTMLInputElement;
+const toFrom = document.querySelector('#to-from') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
-const type2 = document.querySelector('#type') as HTMLInputElement; // casting type
-console.log(type2);
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    console.log(
+        type.value,
+        toFrom.value,
+        details.value,
+        amount.valueAsNumber
+    );
+});
