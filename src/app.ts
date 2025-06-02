48331786fc90d0d82bde913968c213cb39db479e
Isa Reinert <reinert.isa@gmail.com>
2025-06-01 23:27:35 -0700

    feat(website): dom type casting - 4

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 // const anchor = document.querySelector('a')!;
 // console.log(anchor.href);
 
-const form = document.querySelector('form')!;
+// const form = document.querySelector('form')!;
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
